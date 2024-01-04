79e38473a92d04a20e6e51b77c17a6ad9797ef00
jung79human79 <135958821+jung79human79@users.noreply.github.com>
2024-01-04 00:02:28 +0000

    Delete mysql.sql

--- mysql.sql
+++
@@ -1,8 +0,0 @@
-create database bigdata;
-create table student (      
-num int not null primary key auto_increment, 
-name varchar(50),  
-tel varchar(50), 
-age int,        
-email varchar(50)
-);
