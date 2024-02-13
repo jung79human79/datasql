fa54910d01672141758ad7ab0c976bfee4acae04
jung79human79 <wjd1eo2wn3@naver.com>
2024-02-13 14:06:06 +0900

    0213

---
+++ 02. 파이썬 라이브러리/calcal.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def add(num1,num2):
+    return num1+num2
+def minus(num1,num2):
+    return num1-num2
+def mul(num1,num2):
+    return num1*num2
+def div(num1,num2):
+    return num1/num2
+
